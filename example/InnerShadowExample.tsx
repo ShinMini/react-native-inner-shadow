112f538b0fe6323abf184c52b8237ce544f92ee2
ShinMini <gusals121234@gmail.com>
2025-01-22 12:14:00 +0900

    Add InnerShadowExample component to example folder

---
+++ example/InnerShadowExample.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import ShadowView from 'InnerShadowView';
+import LinearShadowView from 'LinearInnerShadowView';
+
+function InnerShadowExample(): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <ShadowView inset style={styles.shadowView}>
+        <Text style={styles.context}>Inner Shadow</Text>
+      </ShadowView>
+
+      <LinearShadowView
+        style={styles.shadowView}
+        colors={['#d3d0c9', '#393939']}
+        from="top"
+        to="right">
+        <Text style={styles.context}>With Linear</Text>
+      </LinearShadowView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fffeede7',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 20,
+  },
+  shadowView: {
+    backgroundColor: '#fffeede7',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 200,
+    height: 200,
+    borderRadius: 14,
+  },
+  context: {
+    fontSize: 24,
+  },
+});
+
+export default InnerShadowExample;
