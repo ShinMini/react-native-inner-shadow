80bc5c08ddb99a939f8a57a568b0fb03c454af08
ShinMini <gusals121234@gmail.com>
2025-01-22 12:27:53 +0900

    Fix typo in app_name string resource -> innershaodw -> innershadow

--- android/app/src/main/java/com/reactnativeinnershaodw/MainApplication.kt
+++ android/app/src/main/java/com/reactnativeinnershaodw/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.reactnativeinnershaodw
+package com.reactnativeinnershadow
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/ReactNativeInnerShadowTests/ReactNativeInnerShadowTests.m
+++ ios/ReactNativeInnerShadowTests/ReactNativeInnerShadowTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeInnerShaodwTests : XCTestCase
+@interface ReactNativeInnerShadowTests : XCTestCase
 
 @end
 
-@implementation ReactNativeInnerShaodwTests
+@implementation ReactNativeInnerShadowTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
