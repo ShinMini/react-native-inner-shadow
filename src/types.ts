dc2f0beb789adaca9366a9b386c06ca57b432486
ShinMini <gusals121234@gmail.com>
2025-01-23 16:17:27 +0900

    feat(constant): fix default value as const

--- src/types.ts
+++ src/types.ts
@@ -8,11 +8,11 @@ import type {PressableProps, ViewStyle} from 'react-native';
  * without getting clipped.
  */
 // export const DEFAULT_SHADOW_SPACE = {dx: 6, dy: 6} as const;
-const DEFAULT_SHADOW_SPACE = 6;
+const DEFAULT_SHADOW_SPACE = 6 as const;
 
 // These two scales are opposite each other to create a "reflected light" effect.
-const DEFAULT_SHADOW_OFFSET_SCALE = 2;
-const DEFAULT_REFLECTED_LIGHT_OFFSET_SCALE = 2;
+const DEFAULT_SHADOW_OFFSET_SCALE = 2 as const;
+const DEFAULT_REFLECTED_LIGHT_OFFSET_SCALE = 2 as const;
 
 /**
  * Default values used when a particular prop isn't provided:
@@ -20,17 +20,18 @@ const DEFAULT_REFLECTED_LIGHT_OFFSET_SCALE = 2;
  *   - DEFAULT_REFLECTED_LIGHT_COLOR: a semi-transparent white for "reflected light" effect
  *   - DEFAULT_SHADOW_COLOR: a dark, semi-transparent color for shadows
  */
-const DEFAULT_BACKGROUND_COLOR = '#FFFFFF';
-const DEFAULT_REFLECTED_LIGHT_COLOR = '#eee9e92d';
-const DEFAULT_SHADOW_COLOR = '#2F2F2FBC';
+const DEFAULT_BACKGROUND_COLOR = '#FFFFFF' as const;
+// const DEFAULT_REFLECTED_LIGHT_COLOR = '#EEE9E92D' as const;
+const DEFAULT_REFLECTED_LIGHT_COLOR = '#6666ff' as const;
+const DEFAULT_SHADOW_COLOR = '#2F2F2FBC' as const;
 
 /**
  * Default shadow blur radius for the main shadow and reflected light.
  * These values control how soft or diffuse the shadow/highlight appears.
  * Higher values create larger, softer shadows.
  * */
-const DEFAULT_SHADOW_BLUR = 3;
-const DEFAULT_REFLECTED_LIGHT_BLUR = 3;
+const DEFAULT_SHADOW_BLUR = 3 as const;
+const DEFAULT_REFLECTED_LIGHT_BLUR = 3 as const;
 
 /**
  * InnerShadowProps defines the basic requirements for
