3d2010f956d95dde49a2dfccb51f3366e4d4a8a3
ShinMini <gusals121234@gmail.com>
2025-02-28 13:39:56 +0900

    chore: update shadow constants and styling parameters

--- src/constants.ts
+++ src/constants.ts
@@ -2,22 +2,27 @@ import { StyleSheet } from 'react-native';
 
 const BACKGROUND_COLOR = '#FFFFFF' as const;
 
-const INITIAL_DEPTH = 3 as const;
+// @deprecated Use `shadowOffset` instead.
+const INITIAL_DEPTH = 2 as const;
 const SHADOW_SPACE = 6 as const;
 
+const SHADOW_OPACITY = 0.3 as const;
+const SHADOW_RADIUS = 3 as const;
 const SHADOW_BLUR = 2 as const;
+const SHADOW_ELEVATION = 3 as const;
+
 const REFLECTED_LIGHT_BLUR = 3 as const;
 
 const SHADOW_COLOR = '#2F2F2FBC' as const;
-const REFLECTED_LIGHT_COLOR = '#EEE9E92D' as const;
+const REFLECTED_LIGHT_COLOR = '#ffffff8d' as const;
 
-const DAMPING_DURATION = 200 as const;
+const DAMPING_DURATION = 150 as const;
 const DAMPING_RATIO = 0.8 as const;
 
 const IS_REFLECTED_LIGHT_ENABLED = true as const;
 
 // These two scales are opposite each other to create a "reflected light" effect.
-const SHADOW_OFFSET_SCALE = 2 as const;
+const SHADOW_OFFSET_SCALE = 2.5 as const;
 const REFLECTED_LIGHT_OFFSET_SCALE = 2 as const;
 
 const COMMON_STYLES = StyleSheet.create({
@@ -37,6 +42,8 @@ export {
   BACKGROUND_COLOR,
   INITIAL_DEPTH,
   SHADOW_SPACE,
+  SHADOW_OPACITY,
+  SHADOW_RADIUS,
   SHADOW_BLUR,
   REFLECTED_LIGHT_BLUR,
   SHADOW_COLOR,
@@ -46,5 +53,6 @@ export {
   IS_REFLECTED_LIGHT_ENABLED,
   SHADOW_OFFSET_SCALE,
   REFLECTED_LIGHT_OFFSET_SCALE,
+  SHADOW_ELEVATION,
   COMMON_STYLES,
 };
